f421bde97901dfd7f07ad14210485af0b3285f2b
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2025-04-28 16:14:29 -0600

    WIP

--- bin/convert.clj
+++ bin/convert.clj
@@ -7,7 +7,18 @@
 (def block-types
   {"disclaimer" "disclaimer"
    "warning" "warning"
+   "quote" "blockquote"
    "note" "note"})
+
+(def heading-levels
+  {1 "part"
+   2 "chapter"
+   3 "section"
+   4 "subsection"
+   5 "subsubsection"})
+
+(defn list-item? [line]
+  (re-matches #"^\s*-\s+.+$" line))
 
 (defn block-start? [line]
   (some->> (re-find #"(?i)^#\+begin_(\w+)" line)
@@ -21,33 +32,77 @@
            (str/lower-case)
            (get block-types)))
 
+(defn heading-line? [line]
+  (re-matches #"^\*+ .+" line))
+
+(defn process-heading [line]
+  (let [m (re-matches #"^(\*+) (.+)" line)
+        stars (count (get m 1))
+        title (get m 2)
+        command (get heading-levels stars)]
+    (str "\\" command "{" title "}")))
+
 (defn convert-inline [line]
-  ;; Simple inline footnote conversion
-  (str/replace line #"\[fn::([^\]]+)\]" "\\\\footnote{$1}"))
+  (-> line
+      ;; Footnotes
+      (str/replace #"\[fn::([^\]]+)\]" "\\\\footnote{$1}")
+      ;; Quotes
+      (str/replace #"“([^”]+)”" "\\\\quotation{$1}")
+      (str/replace #"\"([^\"]+)\"" "\\\\quotation{$1}")
+      ;; Apostrophes
+      (str/replace #"’" "'")
+      ;; Italic: /italic/
+      (str/replace #"(?<!\w)/(.*?)/(?!\w)" "\\\\emph{$1}")
+      ;; Bold: *bold*
+      (str/replace #"(?<!\w)\*(.*?)\*(?!\w)" "\\\\bf {$1}")))
 
 (defn process-lines [lines]
   (loop [lines lines
          out []
          state :normal]
     (if (empty? lines)
-      out
+      ;; if list is open, close it at EOF
+      (if (= state :list)
+        (conj out "\\stopitemize")
+        out)
       (let [line (first lines)
             rest-lines (rest lines)]
         (cond
+          ;; block start
           (block-start? line)
-          (recur rest-lines
-                 (conj out (str "\\start" (block-start? line)))
-                 :block)
+          (let [out (if (= state :list) (conj out "\\stopitemize") out)]
+            (recur rest-lines
+                   (conj out (str "\\start" (block-start? line)))
+                   :block))
 
+          ;; block end
           (block-end? line)
           (recur rest-lines
                  (conj out (str "\\stop" (block-end? line)))
                  :normal)
 
+          ;; heading
+          (heading-line? line)
+          (let [out (if (= state :list) (conj out "\\stopitemize") out)]
+            (recur rest-lines
+                   (conj out (process-heading line))
+                   :normal))
+
+          ;; list item
+          (list-item? line)
+          (let [out (cond
+                      (= state :list) out
+                      :else (conj out "\\startitemize"))]
+            (recur rest-lines
+                   (conj out (str "\\item " (str/trim (subs line 1))))
+                   :list))
+
+          ;; normal line
           :else
-          (recur rest-lines
-                 (conj out (convert-inline line))
-                 state))))))
+          (let [out (if (= state :list) (conj out "\\stopitemize") out)]
+            (recur rest-lines
+                   (conj out (convert-inline line))
+                   :normal)))))))
 
 (defn read-all-chapters [dir]
   (str/join "\n\n" (map (comp slurp str) (sort (fs/glob dir "*.org")))))

1fdf6769dfbe61eff8497ceef450b9b94baef554
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2025-04-28 17:10:16 -0600

    Build translations

--- .env/zsh.zsh
+++ .env/zsh.zsh
@@ -5,6 +5,8 @@ load ~/.zsh/environments/basic.zsh
 alias emacs="emacs --load $PWD/.env/emacs.el"
 alias et="bb -cp src -m jakub-stastny.et.runner"
 
-alias build="./bin/convert.clj > output/content.en.tex && (cd output && context 'Seeds of the New World.tex')"
+alias build.en="./bin/convert.clj > output/content.en.tex && (cd output && context 'Seeds of the New World.tex')"
+alias build.es="./bin/convert.clj > output/content.en.tex && (cd output && context 'Semillas del Nuevo Mundo.tex')"
+alias build.cz="./bin/convert.clj > output/content.en.tex && (cd output && context 'Seminka Noveho Sveta.tex')"
 
 report-custom-functions
